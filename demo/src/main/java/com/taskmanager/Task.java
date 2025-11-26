19e841d208116204ddfbf1e0e0764506a43c9142
gabrielluisnunes <gabrielluisnunes@gmail.com>
2025-11-26 11:31:06 -0300

    feat(model): Create Task domain model

---
+++ demo/src/main/java/com/taskmanager/Task.java
@@ -0,0 +1,46 @@
+package com.taskmanager;
+
+public class Task {
+    // Atributos
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    // Constructor
+    public Task(){
+        this.completed = false;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
