5382efbe3cf4314a7518fd1f942c35f50a15eae9
gabrielluisnunes <gabrielluisnunes@gmail.com>
2025-12-02 10:38:03 -0300

    refactor(lombok): Apply Lombok for boilerplate reduction

--- api-task-manager/src/main/java/com/taskmanager/api_task_manager/Task.java
+++ api-task-manager/src/main/java/com/taskmanager/api_task_manager/Task.java
@@ -1,46 +1,14 @@
 package com.taskmanager.api_task_manager;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
 public class Task {
-    // Atributos
     private Long id;
     private String title;
     private String description;
-    private boolean completed;
-    // Constructor
-    public Task(){
-        this.completed = false;
-    }
-
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
+    private boolean completed = false;
 }
