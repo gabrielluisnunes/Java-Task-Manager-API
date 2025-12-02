9d99090faf399869fad380220074426a9ffa08db
gabrielluisnunes <gabrielluisnunes@gmail.com>
2025-12-02 11:09:19 -0300

    feat(jpa): Implement real database persistence with Spring Data JPA

--- api-task-manager/src/main/java/com/taskmanager/api_task_manager/Task.java
+++ api-task-manager/src/main/java/com/taskmanager/api_task_manager/Task.java
@@ -3,10 +3,18 @@ package com.taskmanager.api_task_manager;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Data
 @NoArgsConstructor
-
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
--- api-task-manager/src/main/java/com/taskmanager/api_task_manager/repository/TaskRepository.java
+++ api-task-manager/src/main/java/com/taskmanager/api_task_manager/repository/TaskRepository.java
@@ -1,38 +1,10 @@
 package com.taskmanager.api_task_manager.repository;
 
-import com.taskmanager.api_task_manager.Task; // Import correto para a classe Task
+import com.taskmanager.api_task_manager.Task; 
 import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public class TaskRepository {
-    //Simulando Banco de Dados com um Map
-    private final Map<Long, Task> tasks = new ConcurrentHashMap<>();
-    private long nextId = 1L;
-
-    //Salvar/Atualizar Tarefa
-    public Task save(Task task) {
-        if (task.getId() == null) {
-            task.setId(nextId++);
-        }
-        tasks.put(task.getId(), task);
-        return task;
-    }
-
-    public Optional<Task> findById(Long id) {
-        return Optional.ofNullable(tasks.get(id));
-    }
-
-    public List<Task> findAll(){
-        return new ArrayList<>(tasks.values());
-    }
-
-    public void deleteById(Long id) {
-        tasks.remove(id);
-    }
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
 }
