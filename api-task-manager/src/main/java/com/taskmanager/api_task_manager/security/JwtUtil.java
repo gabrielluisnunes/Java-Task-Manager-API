18c213c241a17b712ffd1971bb2c453da3b9bea7
gabrielluisnunes <gabrielluisnunes@gmail.com>
2026-01-08 15:55:08 -0300

    feat(auth): implement JwtUtil for token generation and validation

---
+++ api-task-manager/src/main/java/com/taskmanager/api_task_manager/security/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.taskmanager.api_task_manager.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    // Em produção essa chave deve vir de uma variável de ambiente segura
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final int expirationTime = 86400000; 
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
