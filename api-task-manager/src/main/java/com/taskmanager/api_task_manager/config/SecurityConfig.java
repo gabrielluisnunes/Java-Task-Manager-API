4455112613dc57faa9fcff1c2602d8870adc4807
gabrielluisnunes <gabrielluisnunes@gmail.com>
2026-01-12 13:23:08 -0300

    fix: resolve database authentication failure by resetting docker volumes

--- api-task-manager/src/main/java/com/taskmanager/api_task_manager/config/SecurityConfig.java
+++ api-task-manager/src/main/java/com/taskmanager/api_task_manager/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
             
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**","/swagger-ui.html" ).permitAll()
                 .anyRequest().authenticated()
             )
             
